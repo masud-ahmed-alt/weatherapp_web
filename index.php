b8bf311083725b79b4555754b493ac0ab44b195d
Masud Ahmed <ma7332125@gmail.com>
2022-08-10 13:25:12 +0530

    fav icon added

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script src="index.js"></script>
     <title>Weather</title>
+    <link rel="icon" type="image/x-icon" href="res/favicon.png">
 
 </head>
 
